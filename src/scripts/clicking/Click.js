c8f8084194a52cb650ca154faa4d93fb15a88ec4
ColtMay <colten_mayberry@pm.me>
2021-03-12 20:17:29 -0600

    added automatic counter

--- src/scripts/clicking/Click.js
+++ src/scripts/clicking/Click.js
@@ -3,16 +3,25 @@ const applicationElement = document.querySelector(".game");
 let clicks = 0;
 let clickMultiplier = 1;
 
-export const clickCounter = () => {
+export const clickButton = () => {
     applicationElement.addEventListener("click", event => {
         if (event.target.id === "clickButton") {
-        clicks += clickMultiplier;
+        clickCounter();
         updateClickCounter();
         }
     })
 }
 
+const clickCounter = () => {
+    clicks += clickMultiplier;
+}
+
 const updateClickCounter = () => {
     const counter = document.getElementById("clickCounter");
     counter.innerHTML = `Clicks: ${clicks}`;
+}
+
+export const automaticClickCounter = () => {
+    setInterval(clickCounter, 500)
+    setInterval(updateClickCounter, 500)
 }
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,10 @@
-import { clickCounter } from "./clicking/Click.js"
+import { clickButton, automaticClickCounter } from "./clicking/Click.js";
+
 
 const startGame = () => {
-    console.log("The game has started.")
-    clickCounter();
+    console.log("The game has started.");
+    clickButton();
+    automaticClickCounter();
 }
+
 startGame();
